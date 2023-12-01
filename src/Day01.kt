ff49af8e5ae97ae389acebedcbc853f6c9a6c060
Yannick <yannick.pulver@gmail.com>
2023-12-01 10:29:15 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
+    val map = mapOf(
+            "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
+    )
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val result = input.sumOf {
+            val first = it.first(Char::isDigit).digitToInt()
+            val last = it.last(Char::isDigit).digitToInt()
+            first * 10 + last
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val options = map.keys + map.values.map { it.toString() }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val result = input.sumOf {
+            val first = it.findAnyOf(options)?.second.orEmpty().let { map.getOrElse(it) { it.toInt() } }
+            val last = it.findLastAnyOf(options)?.second.orEmpty().let { map.getOrElse(it) { it.toInt() } }
+            first * 10 + last
+        }
+        return result
+    }
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+
