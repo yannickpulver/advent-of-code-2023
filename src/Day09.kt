6b34d5eb6976a0d9230e6e24fd00680fed516b8c
Yannick <yannick.pulver@gmail.com>
2023-12-09 08:57:12 +0100

    cleanup

--- src/Day09.kt
+++ src/Day09.kt
@@ -3,41 +3,25 @@ fun main() {
     val regex = "(\\d|-)+".toRegex()
     fun parse(it: String) = regex.findAll(it).map { it.value.toLong() }.toList()
 
-    fun getLastDiff(values: List<Long>): Long {
-        val diffs = values.mapIndexed { index, i ->
-            i - values[(index - 1).coerceAtLeast(0)]
-        }
-
+    fun diffs(values: List<Long>): Long {
+        val diffs = values.zipWithNext { a, b -> b - a }
         val recDiff = if (diffs.any { it != 0L }) {
-            getLastDiff(diffs.drop(1))
+            diffs(diffs)
         } else 0
-
         return recDiff + diffs.last()
-    }
-
-    fun getFirstDiff(values: List<Long>): Long {
-        val diffs = values.mapIndexed { index, i ->
-            values[(index + 1).coerceAtMost(values.lastIndex)] - i
-        }
-
-        val recDiff = if (diffs.any { it != 0L }) {
-            getFirstDiff(diffs.dropLast(1))
-        } else 0
-
-        return diffs.first() - recDiff
     }
 
     fun part1(input: List<String>): Long {
         return input.sumOf {
             val values = parse(it)
-            getLastDiff(values) + values.last()
+            diffs(values) + values.last()
         }
     }
 
     fun part2(input: List<String>): Any {
         return input.sumOf {
-            val values = parse(it)
-            values.first() - getFirstDiff(values)
+            val values = parse(it).reversed()
+            diffs(values) + values.last()
         }
     }
 
