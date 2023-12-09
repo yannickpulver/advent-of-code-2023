ce05a8e647cb793e7a938ebf1f014183c68d48c6
Yannick <yannick.pulver@gmail.com>
2023-12-09 08:35:52 +0100

    day 9

---
+++ src/DayXY.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+
+
+    fun part1(input: List<String>): Int {
+       return 1
+    }
+
+    fun part2(input: List<String>): Any {
+        return 1
+    }
+
+    val input = readInput("DayXY")
+    part1(input).println()
+    part2(input).println()
+}
+
+
+
